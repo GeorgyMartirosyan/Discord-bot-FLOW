2bd846ca5a1730a8034c9956d772dcdc9ef4a0cc
M3S <143006325+GeorgyMartirosyan@users.noreply.github.com>
2024-08-11 06:45:16 +0300

    new func for_info_contract | new design (+2 gif)

--- cogs/salary_calculator.py
+++ cogs/salary_calculator.py
@@ -7,180 +7,153 @@ from config import Config
 config = Config()
 
 
+# Contract info
+# https://docs.google.com/spreadsheets/d/1eKIc5Fy1jBdKI2HI4DTJt3jEDvuJwMpbEHldppbNAHM/edit?gid=1647271584#gid=1647271584
+
+def for_info_contract(name_contract: str, activation_price: int, family_price: object, amount: int) -> object:
+    name_contract = name_contract
+    activation_price = activation_price
+    family_price = family_price
+    amount = amount
+    return [name_contract, activation_price, family_price, amount]
+
+
 def info_contract(quantity: int, info: str):
     if quantity == 40000 or quantity == 43000 or quantity == 64000 and info == 'fish':
-        name_contract = 'Дары моря I'
-        activation_price = 2000
-        family_price = 30000
-        amount = 87000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря I', activation_price=2000, family_price=config.family_price_fish, amount=87000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 66000 or quantity == 70000 or quantity == 82000 and info == 'fish':
-        name_contract = 'Дары моря II'
-        activation_price = 2000
-        family_price = config.family_price_fish
-        amount = 114000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря II', activation_price=2000, family_price=config.family_price_fish, amount=114000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 122000 or quantity == 136000 or quantity == 138000 and info == 'fish':
-        name_contract = 'Дары моря III'
-        activation_price = 3000
-        family_price = config.family_price_fish
-        amount = 135000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря III', activation_price=3000, family_price=config.family_price_fish, amount=135000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 165000 or quantity == 136000 or quantity == 138000 and info == 'fish':
-        name_contract = 'Дары моря IV'
-        activation_price = 4000
-        family_price = config.family_price_fish
-        amount = 160000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря IV', activation_price=4000, family_price=config.family_price_fish, amount=160000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 158000 or quantity == 163000 or quantity == 167000 and info == 'fish':
-        name_contract = 'Дары моря V'
-        activation_price = 5000
-        family_price = config.family_price_fish
-        amount = 182000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря V', activation_price=5000, family_price=config.family_price_fish, amount=182000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 204000 or quantity == 211000 or quantity == 193000 and info == 'fish':
-        name_contract = 'Дары моря VI'
-        activation_price = 6000
-        family_price = config.family_price_fish
-        amount = 210000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря VI', activation_price=6000, family_price=config.family_price_fish, amount=210000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 213000 or quantity == 226000 or quantity == 251000 and info == 'fish':
-        name_contract = 'Дары моря VII'
-        activation_price = 7000
-        family_price = config.family_price_fish
-        amount = 240000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря VII', activation_price=7000, family_price=config.family_price_fish, amount=240000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 243000 or quantity == 269000 or quantity == 291000 and info == 'fish':
-        name_contract = 'Дары моря VIII'
-        activation_price = 8000
-        family_price = config.family_price_fish
-        amount = 270000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Дары моря VIII', activation_price=8000, family_price=config.family_price_fish, amount=270000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 300 and info == 'metallurgy 1':
-        name_contract = 'Металургия I | Железо'
-        activation_price = 4000
-        family_price = config.family_price_metallurgy
-        amount = 72000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия I | Железо', activation_price=4000,
+            family_price=config.family_price_metallurgy, amount=72000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 150 and info == 'metallurgy 1':
-        name_contract = 'Металургия I | Серебро'
-        activation_price = 4000
-        family_price = config.family_price_metallurgy
-        amount = 90000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия I | Серебро', activation_price=4000,
+            family_price=config.family_price_metallurgy, amount=90000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 75 and info == 'metallurgy 1':
-        name_contract = 'Металургия I | Медь'
-        activation_price = 4000
-        family_price = config.family_price_metallurgy
-        amount = 100000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия I | Медь', activation_price=4000,
+            family_price=config.family_price_metallurgy, amount=100000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 30 and info == 'metallurgy 1':
-        name_contract = 'Металургия I | Олово'
-        activation_price = 4000
-        family_price = config.family_price_metallurgy
-        amount = 110000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия I | Олово', activation_price=4000,
+            family_price=config.family_price_metallurgy, amount=110000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 15 and info == 'metallurgy 1':
-        name_contract = 'Металургия I | Золото'
-        activation_price = 4000
-        family_price = config.family_price_metallurgy
-        amount = 120000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия I | Золото', activation_price=4000,
+            family_price=config.family_price_metallurgy, amount=120000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 400 and info == 'metallurgy 2':
-        name_contract = 'Металургия II | Железо'
-        activation_price = 8000
-        family_price = config.family_price_metallurgy
-        amount = 93600
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия II | Железо', activation_price=8000,
+            family_price=config.family_price_metallurgy, amount=93600)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 200 and info == 'metallurgy 2':
-        name_contract = 'Металургия II | Серебро'
-        activation_price = 8000
-        family_price = config.family_price_metallurgy
-        amount = 117000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия II | Серебро', activation_price=8000,
+            family_price=config.family_price_metallurgy, amount=117000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 100 and info == 'metallurgy 2':
-        name_contract = 'Металургия II | Медь'
-        activation_price = 8000
-        family_price = config.family_price_metallurgy
-        amount = 128700
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия II | Медь', activation_price=8000,
+            family_price=config.family_price_metallurgy, amount=128700)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 40 and info == 'metallurgy 2':
-        name_contract = 'Металургия II | Олово'
-        activation_price = 8000
-        family_price = config.family_price_metallurgy
-        amount = 140400
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия II | Олово', activation_price=8000,
+            family_price=config.family_price_metallurgy, amount=140400)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 20 and info == 'metallurgy 2':
-        name_contract = 'Металургия II | Золото'
-        activation_price = 8000
-        family_price = config.family_price_metallurgy
-        amount = 156700
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия II | Золото', activation_price=8000,
+            family_price=config.family_price_metallurgy, amount=156700)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 500 and info == 'metallurgy 3':
-        name_contract = 'Металургия III | Железо'
-        activation_price = 12000
-        family_price = config.family_price_metallurgy
-        amount = 121600
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия III | Железо', activation_price=12000,
+            family_price=config.family_price_metallurgy, amount=121600)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 250 and info == 'metallurgy 3':
-        name_contract = 'Металургия III | Серебро'
-        activation_price = 12000
-        family_price = config.family_price_metallurgy
-        amount = 152100
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия III | Серебро', activation_price=12000,
+            family_price=config.family_price_metallurgy, amount=152100)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 125 and info == 'metallurgy 3':
-        name_contract = 'Металургия III | Медь'
-        activation_price = 12000
-        family_price = config.family_price_metallurgy
-        amount = 167300
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия III | Медь', activation_price=12000,
+            family_price=config.family_price_metallurgy, amount=167300)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 50 and info == 'metallurgy 3':
-        name_contract = 'Металургия III | Олово'
-        activation_price = 12000
-        family_price = config.family_price_metallurgy
-        amount = 182500
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия III | Олово', activation_price=12000,
+            family_price=config.family_price_metallurgy, amount=182500)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 25 and info == 'metallurgy 3':
-        name_contract = 'Металургия III | Золото'
-        activation_price = 12000
-        family_price = config.family_price_metallurgy
-        amount = 204000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия III | Золото', activation_price=12000,
+            family_price=config.family_price_metallurgy, amount=204000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 600 and info == 'metallurgy 4':
-        name_contract = 'Металургия IV | Железо'
-        activation_price = 16000
-        family_price = config.family_price_metallurgy
-        amount = 158000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия IV | Железо', activation_price=16000,
+            family_price=config.family_price_metallurgy, amount=158000)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 300 and info == 'metallurgy 4':
-        name_contract = 'Металургия IV | Серебро'
-        activation_price = 16000
-        family_price = config.family_price_metallurgy
-        amount = 197700
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия IV | Серебро', activation_price=16000,
+            family_price=config.family_price_metallurgy, amount=197700)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 150 and info == 'metallurgy 4':
-        name_contract = 'Металургия IV | Медь'
-        activation_price = 16000
-        family_price = config.family_price_metallurgy
-        amount = 217500
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия IV | Медь', activation_price=16000,
+            family_price=config.family_price_metallurgy, amount=217500)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 60 and info == 'metallurgy 4':
-        name_contract = 'Металургия IV | Олово'
-        activation_price = 16000
-        family_price = config.family_price_metallurgy
-        amount = 237200
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия IV | Олово', activation_price=16000,
+            family_price=config.family_price_metallurgy, amount=237200)
         return [name_contract, activation_price, family_price, amount]
     elif quantity == 30 and info == 'metallurgy 4':
-        name_contract = 'Металургия IV | Золото'
-        activation_price = 16000
-        family_price = config.family_price_metallurgy
-        amount = 265000
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='Металургия IV | Золото', activation_price=16000,
+            family_price=config.family_price_metallurgy, amount=265000)
         return [name_contract, activation_price, family_price, amount]
     else:
-        name_contract = 'Error'
-        activation_price = 0
-        family_price = 0
-        amount = 0
+        name_contract, activation_price, family_price, amount = for_info_contract(
+            name_contract='None', activation_price=0, family_price=0, amount=0)
         return [name_contract, activation_price, family_price, amount]
 
 
@@ -213,12 +186,13 @@ class SalaryCalculator(commands.Cog):
         )
         embed.set_footer(
             text="by martirosyan",
-            icon_url="https://static.vecteezy.com/system/resources/thumbnails/007/264/787/small/business-finance-logo-"
-                     "template-illustration-free-vector.jpg",
+            icon_url="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzJ4cmpneDZvcmg4a3A3ajRsOXFtdnZnanc1Z3Mwcm5rZnk2MTM"
+                     "zbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mR3FqAStFhyiQ/giphy.gif",
         )
 
         # embed.set_thumbnail(url="https://disnake.dev/assets/disnake-logo.png")
-        # embed.set_image(url="https://images.wsj.net/im-861258?size=1.5005861664712778&width=600&height=400")
+        embed.set_image(url="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2ZkMXJ4aGk2d2dnYzNwcmNmMnBpY2drbmtmNjVndjl"
+                            "kZTJvejRuZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26nfp8HGGHLPGY2KQ/giphy.gif")
 
         embed.add_field(name="Сумма которую вам выплатят:",
                         value=calculation(quantity, count_item, info),
